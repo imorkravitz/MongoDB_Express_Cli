ec352efa5e9716cbe7215d92c6fe36393ffabb34
Or Kravitz <imorkravitz@gmail.com>
2022-01-03 22:46:58 +0200

    not progress

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 var path = require('path');
-
-const dbClient = require("./models/MongoClient");
-const Path = require('./models/paths.model');
-const PathModel = require('./models/paths.model')
-
 
 const app = express();
 app.use(express.json()); 
 
+const dbClient = require("./models/MongoClient");
+const db = require('./models/mongoConnect');
+console.log(dbClient.task+"$$$");
+
+
 app.use('/client', express.static(path.join(__dirname, 'client')));
 app.use('/error', express.static(path.join(__dirname, 'error')));
 
-PathModel.find({},(err,data)=>{
-    if (err) {console.log("Error")}
-    console.log(data);
-})
 
 var countOfScreen = 3;
 app.get('/screen=:num', (req, res) => {
-
-    console.log(Paths)
     var num = req.params.num;
     if(num>0 && num<=countOfScreen){
         res.sendFile(path.join(__dirname, '/client/screen.html'));
@@ -30,5 +24,5 @@ app.get('/screen=:num', (req, res) => {
     }
 })
 
-let port = process.env.PORT || "8083";
+const port = process.env.PORT || "8080";
 app.listen(port, () => console.log(`server is listening on port ${port}`));
--- models/MongoClient.js
+++ models/MongoClient.js
@@ -1,9 +1,10 @@
 const mongodb = require('mongodb');
-const db = require('./mongoData');
+const db = require('./mongoConnect');
 const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://localhost:27017/mydb'
 const dataBaseName = 'mydb';
 const jsonArr = require('../client/jsonData.json');
+const { query } = require('express');
 
 MongoClient.connect(connectionURL, {
     useUnifiedTopology: true,
@@ -15,24 +16,42 @@ MongoClient.connect(connectionURL, {
     console.log('passing the data to Mongo')
     const db = data.db(dataBaseName);
 
-db.collection('jsonDB').insertMany(jsonArr,(err,result) => {
-        if(err){
-            return console.log('error!')
-        }
-        console.log(result.acknowledged)
-    })
-db.collection('jsonDB').find({
+    //adding the advertasing jason to the mongo db collection name jsonDB
+    db.collection('jsonDB').insertMany(jsonArr,(err,result) => {
+            if(err){
+                return console.log('error!')
+            }
+            console.log(result.acknowledged)
+        })
 
-})
+        // db.collection('paths').findOne({
+        //     _id: new mongodb.ObjectID("61d00f222393077dafc164b8")
+        // }, (error,task) => {
+        //     console.log(task.path)
+        // })
 
-db.collection('paths').find({path: "/client/screen.html"}).toArray((error,tasks) => {
-    console.log(tasks)
-})
+    });
 
-// db.collection('paths').findOne({}, (error,task) => {
-//         if (error) throw error;
-//         console.log(task.path)
-//     })
-});
+    function getTypes() {
+            MongoClient.connect(connectionURL, async function (err, db) {
+            let dbo = db.db(dataBaseName);
+            return await dbo.collection("paths").findOne({
+                _id: new mongodb.ObjectID("61d00f222393077dafc164b8")
+            },(err,task)=>{
+                console.log(task.path);
+            });
+        });
+        // module.exports.getTypes = getTypes;
+        // 
+    }
+
+function myFilter(Callback) {
+    if(Callback()){
+        const task = mongoose.model('paths', getTypes)
+        module.exports = task
+    }
+}
+myFilter(getTypes);
+
 exports.MongoClient = MongoClient;
 
