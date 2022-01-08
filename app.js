1fb938f3bfcefcd8f68afc530fb253383ecc9c49
Or Kravitz <imorkravitz@gmail.com>
2022-01-08 14:32:40 +0200

    jqury

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const port = process.env.PORT || "8080";
+const env = require('dotenv');
+env.config();
+const port = process.env.PORT;
 const io = require('socket.io')(http, { cors: {}});
 const path = require('path');
-// app.use(express.json());
-
 
 http.listen(port,() => console.log(`server is listening on port ${port}`));
 
@@ -16,6 +16,7 @@ const {PathModel} = require("./models/PathModel"); // in order to include the sc
 
 //Static files
 
+// app.use(express.json());
 app.use('/client', express.static(path.join(__dirname, 'client')));
 app.use('/error', express.static(path.join(__dirname, 'error')));
 //Socket setup
@@ -24,17 +25,20 @@ io.on('connection', (socket) => {
     console.log('Connected to socket!')
 })
 
+// console.log(db.jsonData());
+
+app.get('/post', (req, res) => {
+    res.send(db.jsonData())
+})
+
 var countOfScreen = 3;
 app.get('/screen=:num', async (req, res) => {
     var num = req.params.num;
-    let data = await PathModel.find({});
-    console.log(data);
-    console.log(data[0]._doc.path);
 
     if(num>0 && num<=countOfScreen){
-        res.sendFile(path.join(__dirname, data[0]._doc.path));
+        res.sendFile(path.join(__dirname, process.env.pathScreen));
     }else{
-        res.sendFile(path.join(__dirname, data[1]._doc.path));
+        res.sendFile(path.join(__dirname, process.env.pathError));
     }
 });
 
